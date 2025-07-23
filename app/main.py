95bda2347f83f61c689ccebcba772b92ed71c69e
Mihirs_Legion <mihirmainkarm3@gmail.com>
2025-07-23 18:20:51 +0100

    backv0.05: Add CORS handling

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from .models import ObjectPose, NBVResponse
 from .nbv_planner import plan_next_best_view
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For testing; lock down for production!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/plan_nbv", response_model=NBVResponse)
 async def plan_nbv(pose: ObjectPose):
